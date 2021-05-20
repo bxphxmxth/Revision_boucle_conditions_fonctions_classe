7033f8c1c49ad26749273b2b8fb6fc5369885757
bxphxmxth <fushs@hotmail.fr>
2021-05-20 21:07:51 +0200

    rev class

--- src/js/main.js
+++ src/js/main.js
@@ -344,17 +344,47 @@
 
 
 
-// // Trouvez un moyen de faire rentrer un mot de passe a un utilisateur avec une boucle for et des conditions. S'il ne trouve pas le mot de passe après 3 essais, affichez-lui "accès refusé", sinon, affichez "accès accordé".
+// // // Trouvez un moyen de faire rentrer un mot de passe a un utilisateur avec une boucle for et des conditions. S'il ne trouve pas le mot de passe après 3 essais, affichez-lui "accès refusé", sinon, affichez "accès accordé".
 
-let rep = prompt("entre un mdp");
+// let rep = prompt("entre un mdp");
 
-for (let i = 0; i < 2; i++){
-     if(rep != "123"){
-          alert("accès refusé, réessaye");
-          let rep = prompt("entre un mdp");
+// for (let i = 0; i < 2; i++){
+//      if(rep != "123"){
+//           alert("accès refusé, réessaye");
+//           let rep = prompt("entre un mdp");
 
           
-     }else {
-          alert("bienvenue");
+//      }else {
+//           alert("bienvenue");
+//      }
+// }
+
+// I.
+// ## EXO1
+// ### Créer une class Personnage avec comme propriétés : 
+// ### age , nom , ville
+// ### Faire 2 instances de cette class.
+
+
+class Personnage {
+     constructor(age, nom, ville){
+          this.age = age;
+          this.nom = nom;
+          this.ville = ville;
+          this.sePresenter = () => {
+               console.log(`bonjour je m'appeller ${this.age}`);
+          }
      }
 }
+
+let naz = new Personnage("naz", 24, "bruxelles");
+let moun = new Personnage("mouna", 35, "bruxelles");
+
+naz.sePresenter();
+moun.sePresenter();
+
+
+// ## EXO2
+// ### Rajouter la methode 'sePresenter' à la class Personnage qui dira "Bonjour, je m'appelle [nom-de-la-personne] !" via un console.log
+// ### Lancer cette méthode sur les deux instances de l'exo1
+
